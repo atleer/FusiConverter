54e909e5ccd8240a118f6cccd6fdfe5e1fb36ccd
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2025-02-05 21:09:09 +0100

    simpler typing

--- scripts/utils.py
+++ scripts/utils.py
@@ -2,16 +2,20 @@ import tkinter as tk
 from tkinter import filedialog
 
 
-def select_files_from_gui(title=None, defaultextension='.mat'):
+def select_files_from_gui(title=None, defaultextension='.mat') -> tuple[str, ...]:
     root = tk.Tk()
     root.withdraw()  # Hide the main window
     file_path = filedialog.askopenfilenames(title=title, defaultextension=defaultextension)  # Open file dialog
+    if not file_path:
+        return ()
     return file_path
 
 
-def select_save_dir_from_gui():
+def select_save_dir_from_gui() -> str | None:
     root = tk.Tk()
     root.withdraw()  # Hide the main window
     file_path = filedialog.askdirectory(title="Save Directory")  # Open folder dialog
+    if not file_path:
+        return None
     return file_path
 
