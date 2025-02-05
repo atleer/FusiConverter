0a18664f87f499e95704d9ba7d05c1c1b6f36403
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2025-02-05 21:09:45 +0100

    param

--- scripts/utils.py
+++ scripts/utils.py
@@ -11,10 +11,10 @@ def select_files_from_gui(title=None, defaultextension='.mat') -> tuple[str, ...
     return file_path
 
 
-def select_save_dir_from_gui() -> str | None:
+def select_save_dir_from_gui(title="Save Directory") -> str | None:
     root = tk.Tk()
     root.withdraw()  # Hide the main window
-    file_path = filedialog.askdirectory(title="Save Directory")  # Open folder dialog
+    file_path = filedialog.askdirectory(title=title)  # Open folder dialog
     if not file_path:
         return None
     return file_path
