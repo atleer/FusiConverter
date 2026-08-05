16b8068deb854418b2b9f175b4cbace17bec7e0d
atleer <atle.rimehaug@gmail.com>
2026-08-05 10:33:56 +0200

    Update paths and imports

--- scripts/view_h5_in_napari.py
+++ scripts/view_h5_in_napari.py
@@ -1,20 +1,26 @@
 import sys
 from pathlib import Path
 
+
 import h5py
 import napari
 import numpy as np
 from tqdm import tqdm
+import os
+from pathlib import Path
 
-from utils import *
-from qtpy.QtWidgets import QVBoxLayout, QWidget, QLabel, QLineEdit, QPushButton
+root_dir = Path(__file__).parent.parent
+os.chdir(root_dir)
+sys.path.insert(0, str(root_dir))
+
+from src.utils import *
+from src.widgets import CropWidget
 
 
 h5_filepaths = select_files_from_gui("Select FuSI H5 Files to View")
 if not h5_filepaths:
     print("No Files Selected.  Quitting...")
     sys.exit()
-
 
 # Load images
 images = {}
@@ -29,36 +35,6 @@ print("Launching Napari Image Viewer...")
 viewer = napari.Viewer()
 for name, image in images.items():
     viewer.add_image(name=name, data=image)
-
-## Crop Widget
-class CropWidget(QWidget):
-    def __init__(self, viewer):
-        super().__init__()
-        self.viewer = viewer
-        self.setLayout(QVBoxLayout())
-
-        self.layout().addWidget(QLabel('Z Range (e.g., 0:10):'))
-        self.z_range_input = QLineEdit()
-        self.layout().addWidget(self.z_range_input)
-
-        self.layout().addWidget(QLabel('X Range (e.g., 0:100):'))
-        self.x_range_input = QLineEdit()
-        self.layout().addWidget(self.x_range_input)
-
-        self.layout().addWidget(QLabel('Y Range (e.g., 0:100):'))
-        self.y_range_input = QLineEdit()
-        self.layout().addWidget(self.y_range_input)
-
-        crop_button = QPushButton('Crop Image & Log-Norm')
-        crop_button.clicked.connect(self.crop)
-        self.layout().addWidget(crop_button)
-
-    def crop(self):
-        z_range = self.z_range_input.text()
-        x_range = self.x_range_input.text()
-        y_range = self.y_range_input.text()
-        crop_image(self.viewer, z_range, x_range, y_range)
-        apply_log_normalization(self.viewer)
 
 
 crop_widget = CropWidget(viewer)
