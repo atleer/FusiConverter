dea2cb6986d6cd903f5520c0b9378a192312297e
atleer <atle.rimehaug@gmail.com>
2026-08-07 16:54:33 +0200

    Create AddLandmark widget upon run

--- scripts/view_h5_in_napari.py
+++ scripts/view_h5_in_napari.py
@@ -14,7 +14,7 @@ os.chdir(root_dir)
 sys.path.insert(0, str(root_dir))
 
 from src.utils import *
-from src.widgets import CropWidget
+from src.widgets import CropWidget, AddLandmark
 
 
 h5_filepaths = select_files_from_gui("Navigate to experiment folder and select fUSI H5 Files to View")
@@ -50,5 +50,8 @@ for name, (image, scale) in images.items():
 
 crop_widget = CropWidget(viewer)
 viewer.window.add_dock_widget(crop_widget, area='right')
+
+add_landmark_widget = AddLandmark(viewer)
+viewer.window.add_dock_widget(add_landmark_widget, area='right')
 
 napari.run()
